66269e9a18418f81f6072d5dc72775d97e3e3ad4
Sabarish.B <76895003+Sabarish2001@users.noreply.github.com>
2022-08-09 23:01:26 +0530

    Create if_elseif_else.R

---
+++ if_elseif_else.R
@@ -0,0 +1,20 @@
+nucleotide <- readline("Enter the nucleotide : ")
+{
+if(nucleotide == "A" | nucleotide == 'a')
+    {
+    print("Adenine")
+}
+else if(nucleotide == "T" | nucleotide == 't')
+    {
+    print("Thymine")
+}
+else if(nucleotide == "G" | nucleotide == 'g')
+    {
+    print("Guanine")
+}
+else if(nucleotide == "C" | nucleotide == 'c')
+    {
+    print("Cytosine")
+}
+else{"OOPS! cant recognize"}
+    }
